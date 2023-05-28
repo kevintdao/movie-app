6fcb3d688dcc5963145453c8825d8676cf236be9
Kevin Dao <kevin-dao@uiowa.edu>
2023-05-27 20:58:20 -0500

    Add click on movie on movie-list page navigate to movie-detail page

--- client/src/app/movies/movie-list/movie-list.component.ts
+++ client/src/app/movies/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from '../movie';
 import { MoviesService } from '../movies.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-list',
@@ -10,11 +11,15 @@ import { MoviesService } from '../movies.service';
 export class MovieListComponent implements OnInit {
   movies: Movie[] = [];
 
-  constructor(private moviesService: MoviesService) {}
+  constructor(private moviesService: MoviesService, private router: Router) {}
 
   ngOnInit(): void {
     this.moviesService.getMovies().subscribe((movies) => {
       this.movies = movies.results;
     });
+  }
+
+  public goToMovieDetails(id: number): void {
+    this.router.navigate(['movies', id]);
   }
 }
